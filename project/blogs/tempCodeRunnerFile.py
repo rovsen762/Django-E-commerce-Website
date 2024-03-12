296d66d4b9077f7cb0e9a71767f2a4b7eabbb3ca
Rovshan Mammadli <63507773+rovsen762@users.noreply.github.com>
2024-03-12 23:14:36 +0400

    Update tempCodeRunnerFile.py

--- project/blogs/tempCodeRunnerFile.py
+++ project/blogs/tempCodeRunnerFile.py
@@ -1,6 +1,1 @@
-secim = input("Lütfen yapmak istediğiniz işlemi belirtin: ")
 
-if(secim=="+"):
-  ilkrakam = int(input("Lütfen ilk rakamı girin: "))
-  ikincirakam = int(input("Lütfen ikinci rakamı girin: "))
-  print(ilkrakam + ikincirakam)
